f66ebd940552e97de5faee5a88dd2442057f247d
jay <jaypifiverr@gmail.com>
2022-09-06 22:57:01 -0400

    commit tree df392552cb3bebf19591870245b01b11bcc132c5

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,26 @@ import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
+import ItemForm from "./ItemForm"
 
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
+  function onItemFormSubmit(newItem) {
+    setItems([...items, newItem])
   }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
       <ShoppingList items={items} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
     </div>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+
+  const [newItem, setNewItem] = useState({
+    id: uuid(),
+    name: "",
+    category: "",
+  })
+
+  function createItems(event) {
+    const { name, value } = event.target
+
+    setNewItem((previousItems) => ({
+      ...previousItems, [name]: value
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    setNewItem({
+      id: uuid(),
+      name: "",
+      category: "",
+    })
+    onItemFormSubmit(newItem)
+    console.log(newItem.name, newItem.category, newItem.id)
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={newItem.name} onChange={createItems}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={createItems}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" >Add to List</button>
     </form>
   );
 }
